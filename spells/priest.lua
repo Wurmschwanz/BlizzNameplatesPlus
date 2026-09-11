015dbe9935ada561490c15912e81a3df5c1d600c
Wurmschwanz <AaronHaendschke@web.de>
2026-09-11 16:49:37 +0200

    Add files via upload

---
+++ spells/priest.lua
@@ -0,0 +1,78 @@
+BNP = BNP or {}
+
+-- Expanded against ShaguPlates enUS debuff tables.
+BNP.PriestAuras = {
+  {
+    key = "shadow_word_pain",
+    names = { "Shadow Word: Pain", "Schattenwort: Schmerz" },
+    duration = 18,
+  },
+  {
+    key = "devouring_plague",
+    names = { "Devouring Plague" },
+    duration = 24,
+  },
+  {
+    key = "vampiric_embrace",
+    names = { "Vampiric Embrace" },
+    duration = 60,
+  },
+  {
+    key = "mind_flay",
+    channel = true,
+    names = { "Mind Flay" },
+    duration = 3,
+  },
+  {
+    key = "psychic_scream",
+    names = { "Psychic Scream" },
+    duration = 8,
+  },
+  {
+    key = "silence",
+    names = { "Silence" },
+    duration = 5,
+  },
+  {
+    key = "hex_of_weakness",
+    names = { "Hex of Weakness" },
+    duration = 120,
+  },
+  {
+    key = "touch_of_weakness",
+    names = { "Touch of Weakness" },
+    duration = 120,
+  },
+  {
+    key = "holy_fire",
+    names = { "Holy Fire" },
+    duration = 10,
+  },
+  {
+    key = "blackout",
+    names = { "Blackout" },
+    duration = 3,
+  },
+  {
+    key = "starshards",
+    channel = true,
+    names = { "Starshards" },
+    duration = 6,
+  },
+  {
+    key = "shadow_vulnerability",
+    names = { "Shadow Vulnerability" },
+    duration = 15,
+  },
+  {
+    key = "shackle_undead",
+    names = { "Shackle Undead" },
+    duration = 50,
+  },
+  {
+    key = "mind_control",
+    channel = true,
+    names = { "Mind Control" },
+    duration = 60,
+  },
+}
