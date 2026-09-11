c1681033b7945fa9de10ac7e26222daf9bc2ad34
Wurmschwanz <AaronHaendschke@web.de>
2026-09-11 18:37:52 +0200

    Add files via upload

---
+++ spells/hunter.lua
@@ -0,0 +1,97 @@
+BNP = BNP or {}
+
+-- Expanded against ShaguPlates enUS debuff tables.
+BNP.HunterAuras = {
+  {
+    key = "immolation_trap_effect",
+    names = { "Immolation Trap Effect" },
+    duration = 15,
+    spellIDs = { 14298 },
+  },
+  {
+    key = "explosive_trap_effect",
+    names = { "Explosive Trap Effect" },
+    duration = 20,
+  },
+  {
+    key = "freezing_trap_effect",
+    names = { "Freezing Trap Effect" },
+    duration = 20,
+  },
+  {
+    key = "serpent_sting",
+    names = { "Serpent Sting" },
+    duration = 15,
+  },
+  {
+    key = "viper_sting",
+    names = { "Viper Sting" },
+    duration = 8,
+  },
+  {
+    key = "scorpid_sting",
+    names = { "Scorpid Sting" },
+    duration = 20,
+  },
+  {
+    key = "improved_scorpid_sting",
+    names = { "Improved Scorpid Sting" },
+    duration = 20,
+  },
+  {
+    key = "wing_clip",
+    names = { "Wing Clip" },
+    duration = 10,
+  },
+  {
+    key = "improved_wing_clip",
+    names = { "Improved Wing Clip" },
+    duration = 5,
+  },
+  {
+    key = "concussive_shot",
+    names = { "Concussive Shot" },
+    duration = 4,
+  },
+  {
+    key = "improved_concussive_shot",
+    names = { "Improved Concussive Shot" },
+    duration = 3,
+  },
+  {
+    key = "scatter_shot",
+    names = { "Scatter Shot" },
+    duration = 4,
+  },
+  {
+    key = "wyvern_sting",
+    names = { "Wyvern Sting" },
+    duration = 12,
+  },
+  {
+    key = "entrapment",
+    names = { "Entrapment" },
+    duration = 5,
+  },
+  {
+    key = "counterattack",
+    names = { "Counterattack" },
+    duration = 5,
+  },
+  {
+    key = "intimidation",
+    names = { "Intimidation" },
+    duration = 3,
+  },
+  {
+    key = "scare_beast",
+    names = { "Scare Beast" },
+    duration = 20,
+  },
+  {
+    key = "hunters_mark",
+    names = { "Hunter's Mark" },
+    duration = 120,
+    shared = true,
+  },
+}
